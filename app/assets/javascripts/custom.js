cd2c7cf6b8654d55129c31fd6965283f88832590
kouchchivorn <kouchchivorn5@yahoo.com>
2014-12-05 23:06:53 +0700

    create lesson page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,35 @@
-$(function(){
-    empty_side = $(".table").find(".empty_side");
+function caculateHeight(){
     var i=0;
-    var category_side = $(".table").find(".category_side").each(function(){
-        empty_side.eq(i).height($(this).height());
+    $(".table").find(".category_side").each(function(){
+        $(".table").find(".empty_side").eq(i).height($(this).height());
         i++;
     });
+}
+
+$(document).on("page:change",function(){
+    $("#Category").on("click", function(){
+    });
+    $("#Category").trigger("click");
+    caculateHeight();
 });
+
+$(document).on("page:change", function(){
+    $(".word_answer").on("click",function(e){
+        $this = $(this);
+        $this.parent().hide().next().next().show();
+        $this.css("background-color", "red");
+        e.preventDefault();
+    });
+    $(".next").on("click",function(e){
+        $(this).parent().hide().next().next().show();
+        e.preventDefault();
+    });
+    $(".previous").on("click",function(e){
+        $(this).parent().hide().prev().prev().show();
+        e.preventDefault();
+    });
+});
+
+
+
 
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,7 @@ class Lesson < ActiveRecord::Base
     belongs_to :user
     belongs_to :category
     has_many :lesson_words, dependent: :destroy
+    accepts_nested_attributes_for :lesson_words
     validates :user_id, presence: true
     validates :category_id, presence: true
 
