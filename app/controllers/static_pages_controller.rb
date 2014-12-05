58df05d7fa47b9137189fa7316f3042986b901a2
kouchchivorn <kouchchivorn5@yahoo.com>
2014-12-05 23:06:53 +0700

    create user show page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+     @lessons = current_user.lessons.order(updated_at: :desc)
+                        .paginate page: params[:page], per_page: 20
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-
   root 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'contact'  => 'static_pages#contact'
@@ -14,4 +12,5 @@ Rails.application.routes.draw do
   resources :categories
   resources :lessons
   resources :results
+  resources :activities
 end
